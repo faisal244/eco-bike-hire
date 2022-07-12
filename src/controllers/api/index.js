e18e7699d4db72a08903af410fb4f55430404d67
Amalidi <A.idi12@outlook.com>
2022-07-12 20:57:33 +0100

    added the bookings

--- src/controllers/api/index.js
+++ src/controllers/api/index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 const Bike = require("../../models/Bike");
+const Booking = require("../../models/Booking");
 
 const getAllBikes = async (req, res) => {
   try {
@@ -16,45 +17,51 @@ const getAllBikes = async (req, res) => {
 };
 
 const getSingleBike = async (req, res) => {
-  return res.render("getSingleBikes");
-  // try {
-  // //   const { id } = req.params;
-  // //   const data = await Playlist.findByPk(id);
+  try {
+    const { id } = req.params;
 
-  // //   if (!data) {
-  // //     return res.status(404).json({ success: false });
-  // //   }
+    const data = await Bike.findByPk(id);
 
-  // //   return res.json({ success: true, data });
-  // // } catch (error) {
-  // //   console.log(`[ERROR]: Failed to get all playlists | ${error.message}`);
+    if (!data) {
+      return res.status(404).json({ success: false });
+    }
 
-  // //   return res.status(500).json({ success: false });
-  // }
+    return res.json({ success: true, data });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get single bike | ${error.message}`);
+
+    return res.status(500).json({ success: false });
+  }
 };
 
 const getAllBookings = async (req, res) => {
-  // try {
-  // } catch (error) {
-  //   console.log(`[ERROR]: Failed to get all playlists | ${error.message}`);
+  try {
+    const data = await Booking.findAll({});
 
-  //   return res.status(500).json({ success: false });
-  // }
-  return res.render("getAllBookings");
+    return res.json({ success: true, data });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get all bookings | ${error.message}`);
+
+    return res.status(500).json({ success: false });
+  }
 };
+
 const getSingleBooking = async (req, res) => {
-  // try {
-  //   const { id } = req.params;
-  //   const data = await Playlist.findByPk(id);
-  //   if (!data) {
-  //     return res.status(404).json({ success: false });
-  //   }
-  //   return res.json({ success: true, data });
-  // } catch (error) {
-  //   console.log(`[ERROR]: Failed to get all playlists | ${error.message}`);
-  //   return res.status(500).json({ success: false });
-  // }
-  return res.render("getSingleBooking");
+  try {
+    const { id } = req.params;
+
+    const data = await Booking.findByPk(id);
+
+    if (!data) {
+      return res.status(404).json({ success: false });
+    }
+
+    return res.json({ success: true, data });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get single booking | ${error.message}`);
+
+    return res.status(500).json({ success: false });
+  }
 };
 
 module.exports = {
