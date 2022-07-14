5cc304da13536eb5689b5de6a2b77e07a9724287
abdinasir1993 <abdinasir_jirde@hotmail.com>
2022-07-14 18:59:30 +0100

    error message fixed

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,15 +1,15 @@
-const signupForm = $("#signup-form");
-const loginForm = $("#login-form");
-const errorText = $("#error-text");
+const signupForm = $('#signup-form');
+const loginForm = $('#login-form');
+const errorText = $('#error-text');
 
 const handleSignup = async (event) => {
   event.preventDefault();
 
-  const firstName = $("#firstName").val();
-  const lastName = $("#lastName").val();
-  const email = $("#email").val();
-  const password = $("#password").val();
-  const confirmPassword = $("#confirmPassword").val();
+  const firstName = $('#firstName').val();
+  const lastName = $('#lastName').val();
+  const email = $('#email').val();
+  const password = $('#password').val();
+  const confirmPassword = $('#confirmPassword').val();
 
   errorText.empty();
 
@@ -23,44 +23,38 @@ const handleSignup = async (event) => {
           password,
         };
 
-        const response = await fetch("/auth/signup", {
-          method: "POST",
+        const response = await fetch('/auth/signup', {
+          method: 'POST',
           body: JSON.stringify(payload),
           headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
           },
         });
 
         const data = await response.json();
         if (data.success) {
-          window.location.assign("/login");
+          window.location.assign('/login');
         } else {
           errorText.append(
-            `<p class="text-danger">error message: failed to create account</p>`
+            `<p class="text-danger">Failed to create account</p>`
           );
         }
       } catch (error) {
-        errorText.append(
-          `<p class="text-danger">error message: failed to create account</p>`
-        );
+        errorText.append(`<p class="text-danger">Failed to create account</p>`);
       }
     } else {
-      errorText.append(
-        `<p class="text-danger">error message: passwords do not match</p>`
-      );
+      errorText.append(`<p class="text-danger">Passwords do not match</p>`);
     }
   } else {
-    errorText.append(
-      `<p class="text-danger">error message: please complete all fields</p>`
-    );
+    errorText.append(`<p class="text-danger">Please complete all fields</p>`);
   }
 };
 
 const handleLogin = async (event) => {
   event.preventDefault();
 
-  const email = $("#email").val();
-  const password = $("#password").val();
+  const email = $('#email').val();
+  const password = $('#password').val();
 
   errorText.empty();
 
@@ -71,32 +65,26 @@ const handleLogin = async (event) => {
         password,
       };
 
-      const response = await fetch("/auth/login", {
-        method: "POST",
+      const response = await fetch('/auth/login', {
+        method: 'POST',
         body: JSON.stringify(payload),
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
       });
 
       const data = await response.json();
 
       if (data.success) {
-        window.location.assign("/dashboard");
+        window.location.assign('/dashboard');
       } else {
-        errorText.append(
-          `<p class="text-danger">error message: failed to login</p>`
-        );
+        errorText.append(`<p class="text-danger">Failed to login</p>`);
       }
     } catch (error) {
-      errorText.append(
-        `<p class="text-danger">error message: failed to login</p>`
-      );
+      errorText.append(`<p class="text-danger">Failed to login</p>`);
     }
   } else {
-    errorText.append(
-      `<p class="text-danger">error message: please complete all fields</p>`
-    );
+    errorText.append(`<p class="text-danger">Please complete all fields</p>`);
   }
 };
 
