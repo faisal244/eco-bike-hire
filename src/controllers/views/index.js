e808970c072e52607e529405358beb439aaf7d74
Faisal Mohammed <40631753+faisal244@users.noreply.github.com>
2022-07-16 12:02:48 +0100

    Redesigned all bikes page
    - New Card design based on bootstrap 5
    
    - bikes.handlebars file currently being worked on
    
    - Created new view route for rendering all bikes
    
    -

--- src/controllers/views/index.js
+++ src/controllers/views/index.js
@@ -1,49 +1,61 @@
 const moment = require("moment");
 
 const { Bike, Pricing } = require("../../models");
+const { getAllBikes } = require("../api/bikes");
 
 const renderHomePage = (req, res) => {
-  return res.render("home");
+	return res.render("home");
 };
 
 const renderLoginPage = (req, res) => {
-  return res.render("login");
+	return res.render("login");
 };
 
 const renderSignupPage = (req, res) => {
-  return res.render("signup", { currentPage: "signup" });
+	return res.render("signup", { currentPage: "signup" });
 };
 
 const renderBookingsPage = (req, res) => {
-  return res.render("bookings");
+	return res.render("bookings");
 };
 
 const renderBikePage = async (req, res) => {
-  const { id } = req.params;
+	const { id } = req.params;
 
-  const bikeFromDb = await Bike.findByPk(id, {
-    include: [
-      {
-        model: Pricing,
-      },
-    ],
-  });
+	const bikeFromDb = await Bike.findByPk(id, {
+		include: [
+			{
+				model: Pricing,
+			},
+		],
+	});
 
-  const bike = bikeFromDb.get({
-    plain: true,
-  });
+	const bike = bikeFromDb.get({
+		plain: true,
+	});
 
-  return res.render("bike", {
-    bike,
-    isLoggedIn: req.session.isLoggedIn,
-    currentDate: moment().format("YYYY-MM-DD"),
-  });
+	return res.render("bike", {
+		bike,
+		isLoggedIn: req.session.isLoggedIn,
+		currentDate: moment().format("YYYY-MM-DD"),
+	});
+};
+
+const renderAllBikes = async (req, res) => {
+	const bikes = await Bike.findAll({});
+
+	// const bike = bike.get({
+	// 	plain: true,
+	// });
+	console.log(bikes);
+	return res.render("bikes", bikes);
 };
 
 module.exports = {
-  renderHomePage,
-  renderLoginPage,
-  renderSignupPage,
-  renderBookingsPage,
-  renderBikePage,
+	renderHomePage,
+	renderLoginPage,
+	renderSignupPage,
+	renderBookingsPage,
+	renderBikePage,
+	renderAllBikes,
 };
