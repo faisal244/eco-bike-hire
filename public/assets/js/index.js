741cc8c78ab73a5be90018113ea3b10f6377f6de
smeea-2018 <smeeaarshad@gmail.com>
2022-07-17 22:38:09 +0100

    code fix for creating a booking by adding bikeId in payload and changing the data value according to database

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,17 +1,17 @@
-const signupForm = $('#signup-form');
-const loginForm = $('#login-form');
-const errorText = $('#error-text');
-const bookingform = $('#booking-form');
+const signupForm = $("#signup-form");
+const loginForm = $("#login-form");
+const errorText = $("#error-text");
+const bookingForm = $("#booking-form");
 //const bookForm = document.getElementById("booking-form");
 
 const handleSignup = async (event) => {
   event.preventDefault();
 
-  const firstName = $('#firstName').val();
-  const lastName = $('#lastName').val();
-  const email = $('#email').val();
-  const password = $('#password').val();
-  const confirmPassword = $('#confirmPassword').val();
+  const firstName = $("#firstName").val();
+  const lastName = $("#lastName").val();
+  const email = $("#email").val();
+  const password = $("#password").val();
+  const confirmPassword = $("#confirmPassword").val();
 
   errorText.empty();
 
@@ -25,17 +25,17 @@ const handleSignup = async (event) => {
           password,
         };
 
-        const response = await fetch('/auth/signup', {
-          method: 'POST',
+        const response = await fetch("/auth/signup", {
+          method: "POST",
           body: JSON.stringify(payload),
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
         });
 
         const data = await response.json();
         if (data.success) {
-          window.location.assign('/login');
+          window.location.assign("/login");
         } else {
           errorText.append(
             `<p class="text-danger">Failed to create account</p>`
@@ -55,8 +55,8 @@ const handleSignup = async (event) => {
 const handleLogin = async (event) => {
   event.preventDefault();
 
-  const email = $('#email').val();
-  const password = $('#password').val();
+  const email = $("#email").val();
+  const password = $("#password").val();
 
   errorText.empty();
 
@@ -67,18 +67,18 @@ const handleLogin = async (event) => {
         password,
       };
 
-      const response = await fetch('/auth/login', {
-        method: 'POST',
+      const response = await fetch("/auth/login", {
+        method: "POST",
         body: JSON.stringify(payload),
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
       });
 
       const data = await response.json();
 
       if (data.success) {
-        window.location.assign('/dashboard');
+        window.location.assign("/dashboard");
       } else {
         errorText.append(`<p class="text-danger">Failed to login</p>`);
       }
@@ -90,51 +90,47 @@ const handleLogin = async (event) => {
   }
 };
 
-const handleBooking = async (event) => {
-  console.log('hello');
-};
-
 const handleCreateBooking = async (event) => {
   event.preventDefault();
 
   errorText.empty();
+  const bookingType = $("#bookingType").val();
+  const duration = $("#booking-duration").val();
+  const startDate = $("#startDate").val();
 
-  const bookingType = $('#bookingType').val();
-  const bookingDuration = $('#booking-duration').val();
-  const startDate = $('#startDate').val();
-
-  if (bookingType && bookingDuration && startDate) {
+  if (bookingType && duration && startDate) {
     try {
       const payload = {
-        bookingType,
-        bookingDuration,
+        bikeId: event.target.dataset.value,
+        isDaily: bookingType === "daily" ? true : false,
+        duration,
         startDate,
       };
 
-      const response = await fetch('/api/bookings', {
-        method: 'POST',
+      const response = await fetch("/api/bookings", {
+        method: "POST",
         body: JSON.stringify(payload),
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
       });
 
       const data = await response.json();
 
       if (data.success) {
-        window.location.assign('/dashboard');
+        window.location.assign("/dashboard");
         console.log / success;
       } else {
-        errorText.append('Failed to create a new booking2. Please try again.');
+        errorText.append("Failed to create a new booking2. Please try again.");
       }
     } catch (error) {
-      errorText.append('Failed to create a new booking1. Please try again.');
+      errorText.append("Failed to create a new booking1. Please try again.");
     }
   } else {
-    errorText.append('Please complete all required fields.');
+    errorText.append("Please complete all required fields.");
   }
 };
 
 signupForm.submit(handleSignup);
 loginForm.submit(handleLogin);
-bookingform.submit(handleCreateBooking);
+bookingForm.submit(handleCreateBooking);
